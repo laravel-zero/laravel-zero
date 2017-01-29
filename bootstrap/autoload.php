3883aeafa21a7996a9bd981d69af0132996fd9c7
Nuno Maduro <enunomaduro@gmail.com>
2017-01-29 18:06:56 +0100

    Fixes autoload on global installation

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -12,4 +12,8 @@
 |
 */
 
-require __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+} else {
+    require __DIR__.'/../../autoload.php';
+}
