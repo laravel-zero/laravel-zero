0c4f789b02c2206fc6b33b526af7ca7fa02483ba
Nuno Maduro <enunomaduro@gmail.com>
2017-01-29 18:19:36 +0100

    Fixes include

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -15,5 +15,5 @@
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require __DIR__ . '/../vendor/autoload.php';
 } else {
-    require __DIR__.'/../../autoload.php';
+    require __DIR__.'/../../../autoload.php';
 }
