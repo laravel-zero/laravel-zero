449474f09bd9478e47a709893e5fb23998e1ae7b
Nuno Maduro <enunomaduro@gmail.com>
2017-02-06 01:16:29 +0100

    Adds base unit test for build

---
+++ tests/Unit/BuildCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Console\Commands\Build;
+
+class BuildCommandTest extends TestCase
+{
+    /**
+     * The build command test.
+     *
+     * @todo Implement this test
+     * @return void
+     */
+    public function testCall()
+    {
+        $command = new Build;
+        $this->assertTrue(true);
+    }
+}
