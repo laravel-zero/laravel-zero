50f3b680015ca716c12e82d6df208c14d600d139
Nuno Maduro <enunomaduro@gmail.com>
2017-04-06 23:16:55 +0200

    Adds compatibility to Laravel Service Providers

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Your application configuration
+    |--------------------------------------------------------------------------
+    | Configuration your application. You may want to introduce the
+    | configuration associated to each service provider that you need.
+    |
+    | Assuming that you are using the service provider EncryptionServiceProvider
+    | you may want to configure the key and the cipher. Take a look
+    | on the example below.
+    */
+    'app' => [
+        'key' => 'base64:0h6bUwJcoLAhKfX2opWKmrQTYJ7hqyhbY5auCwpU38M=',
+        'cipher' => 'AES-256-CBC',
+    ],
+];
