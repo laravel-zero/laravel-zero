1171d1db83c635250c553145b04ff4c5ee6759d1
Nuno Maduro <enunomaduro@gmail.com>
2017-04-07 00:11:47 +0200

    Fixes config

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ return [
     |--------------------------------------------------------------------------
     | Your application configuration
     |--------------------------------------------------------------------------
-    | Configuration your application. You may want to introduce the
+    | Configure your application! You may want to introduce the
     | configuration associated to each service provider that you need.
     |
     | Assuming that you are using the service provider EncryptionServiceProvider
@@ -14,7 +14,7 @@ return [
     | on the example below.
     */
     'app' => [
-        'key' => 'base64:0h6bUwJcoLAhKfX2opWKmrQTYJ7hqyhbY5auCwpU38M=',
-        'cipher' => 'AES-256-CBC',
+        // 'key' => 'base64:0h6bUwJcoLAhKfX2opWKmrQTYJ7hqyhbY5auCwpU38M=',
+        // 'cipher' => 'AES-256-CBC',
     ],
 ];
