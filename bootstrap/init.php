d7b55f01ca4650a41558ffbd22470015b3fb1715
Nuno Maduro <enunomaduro@gmail.com>
2017-07-02 23:38:00 +0200

    Fixes application build (#18)

---
+++ bootstrap/init.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__.'/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Console Application
+|--------------------------------------------------------------------------
+|
+| When we run the console application, the current CLI command will be
+| executed in this console and the response sent back to a terminal
+| or another output device for the developers. Here goes nothing!
+|
+*/
+
+(new App\Console\Application)->run();
