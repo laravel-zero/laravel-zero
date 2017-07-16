8ad6896174d63a576e9638fe25a35e9960a38019
Nuno Maduro <enunomaduro@gmail.com>
2017-07-16 14:06:03 +0000

    Apply fixes from StyleCI

--- config/config.php
+++ config/config.php
@@ -4,24 +4,24 @@ use NunoMaduro\ZeroFramework;
 
 return [
 
-    /**
+    /*
      * Here goes your console application configuration. You
      * may define default commands or even Laravel Service
      * Providers configuration.
      */
     'app' => [
 
-        /**
+        /*
          * Here goes the application name.
          */
         'name' => 'Laravel Zero',
 
-        /**
+        /*
          * Here goes the application version.
          */
-        'version' => "1.0.0",
+        'version' => '1.0.0',
 
-        /**
+        /*
          * The application default command.
          *
          * You may want to remove this line in order to ask the user what command he
@@ -29,7 +29,7 @@ return [
          */
         'default-command' => App\DefaultCommand::class,
 
-        /**
+        /*
          * The application list of commands. Besides the default command the user can
          * also call any of the commands specified below.
          */
@@ -38,7 +38,7 @@ return [
             ZeroFramework\Commands\Renamer::class,
         ],
 
-        /**
+        /*
          * Your application service providers. You may want work with an
          * external Laravel package.
          */
