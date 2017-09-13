42a8172c412a95eeaed8a66a77f201172f7f0394
Nuno Maduro <enunomaduro@gmail.com>
2017-09-13 22:55:52 +0200

    Moves from Unit to Integration approach

--- tests/Integration/HelloCommandTest.php
+++ tests/Integration/HelloCommandTest.php
@@ -7,12 +7,8 @@ use App\Commands\HelloCommand;
 
 class HelloCommandTest extends TestCase
 {
-    /**
-     * The hello command test example.
-     *
-     * @return void
-     */
-    public function testCall(): void
+    /** @test */
+    public function it_checks_the_hello_command_output(): void
     {
         $this->app->call((new HelloCommand())->getName());
 
