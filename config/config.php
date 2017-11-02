34445102600448ed36fc540c3c9e730c1a94f688
Nuno Maduro <enunomaduro@gmail.com>
2017-11-02 22:02:47 +0100

    Removes database config from default config

--- config/config.php
+++ config/config.php
@@ -38,28 +38,4 @@ return [
             App\Providers\AppServiceProvider::class,
         ],
     ],
-
-    /*
-     * Here goes the illuminate/database component configuration. Once
-     * installed, the configuration below is used to configure your
-     * database component.
-     */
-    'database' => [
-        /*
-         * If true, migrations commands will be available.
-         */
-        'with-migrations' => true,
-
-        /*
-         * Here goes the application database connection configuration. By
-         * default, we use `sqlite` as a driver. Feel free to use another
-         * driver, be sure to check the database component documentation.
-         */
-        'connections' => [
-            'default' => [
-                'driver' => 'sqlite',
-                'database' => __DIR__.'/../database/database.sqlite',
-            ],
-        ],
-    ],
 ];
