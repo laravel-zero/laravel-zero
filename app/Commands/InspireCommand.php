993d745105a4c3d1fe3b45cf5270619e0384c978
Owen Voke <development@voke.dev>
2023-02-06 14:12:30 +0000

    chore: resolve incorrect return types

--- app/Commands/InspireCommand.php
+++ app/Commands/InspireCommand.php
@@ -24,8 +24,6 @@ class InspireCommand extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle(): void
     {
--- tests/Pest.php
+++ tests/Pest.php
@@ -39,7 +39,7 @@ expect()->extend('toBeOne', function () {
 |
 */
 
-function something()
+function something(): void
 {
     // ..
 }
