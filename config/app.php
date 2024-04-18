30e341db72efa657faa6fbfa916fb350be9063b3
Nuno Maduro <enunomaduro@gmail.com>
2024-04-18 19:54:57 +0100

    Adds support for Laravel 11

--- config/app.php
+++ config/app.php
@@ -44,19 +44,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. We have gone
-    | ahead and set this to a sensible default for you out of the box.
-    |
-    */
-
-    'timezone' => 'UTC',
-
-    /*
-    |--------------------------------------------------------------------------
     | Autoloaded Service Providers
     |--------------------------------------------------------------------------
     |
